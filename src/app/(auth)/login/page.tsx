d6e9cd377210775a2fbb6cd17cb99c34ac0a2a06
tech-sensei <ibraheemtobiloba15@gmail.com>
2026-09-02 21:24:17 +0100

    fixes...

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -19,7 +19,7 @@ const Login = () => {
   const searchParams = useSearchParams();
   const dispatch = useDispatch();
 
-  const [formData, setFormData] = useState({ email: "charlesayom@gmail.com", password: "Password@1234" });
+  const [formData, setFormData] = useState({ email: "cayomike@gmail.com", password: "SuperAdmin123!" });
   const [rememberMe, setRememberMe] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState<Record<string, string>>({ email: "", password: "" });
